72ebb36066cad5a83c0dceba6ce73b3b2ed12e56
Javier Bértoli <javier@netmanagers.com.ar>
2018-12-27 19:41:33 -0300

    Update tests to use docker and inspec
    Add travis support, some other minor changes.

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -1,6 +1,10 @@
-firewall formula
+iptables formula
 ================
 
-0.1 (01-05-2014)
+0.1 (2014-05-01)
 
 - Initial version with support just for iptables
+
+0.1.1 (2018-12-27)
+
+- Update kitchen testing
---
+++ test/integration/default/packages_spec.rb
@@ -0,0 +1,16 @@
+common_packages = ['iptables']
+
+case os[:name]
+when 'debian', 'ubuntu'
+  all_packages = common_packages + ['iptables-persistent']
+else
+  all_packages = common_packages
+end
+
+control 'Packages' do
+  all_packages.each do |p|
+    describe package(p) do
+      it { should be_installed }
+    end
+  end
+end
